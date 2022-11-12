0a23dcc4a788547e2b991916e7c1bd3d7fccd183
JavierHdzO <fco_hernandez1705@hotmail.com>
2022-11-11 19:50:48 -0600

    11-11-22F feat: add create, read operations of project module

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -10,9 +10,9 @@ import axios from 'axios'
 
 
 // const api = axios.create({ baseURL: 'https://be-booking-platform-production.up.railway.app/api' })
-const api = axios.create({ baseURL: 'http://localhost:3000/api' })
+// const api = axios.create({ baseURL: 'http://localhost:3000/api' })
 
-// const api = axios.create({ baseURL: 'https://be-booking-platform-a35w0mdfk-javierhdzo.vercel.app/api' })
+const api = axios.create({ baseURL: 'https://be-booking-platform-a35w0mdfk-javierhdzo.vercel.app/api' })
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
